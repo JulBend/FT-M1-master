ef66fa2a3cd9244a226458b886e5ef22af6a3e8c
JulBend <juliobc123cesar@gmail.com>
2022-06-29 15:35:04 -0300

    Primer cambio

---
+++ FT-M1-master/01-IntroToCS/homework/homework.js
@@ -0,0 +1,23 @@
+'use strict'
+
+function BinarioADecimal(num) {
+  // tu codigo aca
+var decimal = 0;
+for(var i = 0; i < num.length; i++){
+  decimal = decimal + num[i] * 2 ** (num.length - 1 - i) 
+}return decimal
+}
+
+function DecimalABinario(num) {
+  // tu codigo aca
+  var binario = '';
+  while(num >= 1){
+    binario = num % 2 + binario;
+    num = Math.floor(num / 2);
+  }
+return binario
+}
+module.exports = {
+  BinarioADecimal,
+  DecimalABinario,
+}
